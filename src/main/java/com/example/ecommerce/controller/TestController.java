95b126296bcbffebd23beca17b92c9b7f6787e5a
nhh75 <nguyenhoanghai0507@gmail.com>
2022-10-13 21:48:11 +0700

    test deploy

--- src/main/java/com/example/ecommerce/controller/TestController.java
+++ src/main/java/com/example/ecommerce/controller/TestController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class TestController {
-    @GetMapping("/index")
+    @GetMapping("")
     public String index(){
         return "Nguyen Hoang Hai";
     }
