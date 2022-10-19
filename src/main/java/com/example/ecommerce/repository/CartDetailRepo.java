0a58d1dd90bbfc7627a54504d0743ba127da666a
nhh75 <nguyenhoanghai0507@gmail.com>
2022-10-19 21:47:21 +0700

    add login and register

--- src/main/java/com/example/ecommerce/repository/CartDetailRepo.java
+++ src/main/java/com/example/ecommerce/repository/CartDetailRepo.java
@@ -4,7 +4,7 @@ import com.example.ecommerce.model.CartDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CartDetailRepo extends JpaRepository<Integer, CartDetail> {
+public interface CartDetailRepo extends JpaRepository<CartDetail,Integer> {
     @Query(value="SELECT * FROM cart WHERE cart_id=?1",nativeQuery = true)
     CartDetail getCartDetail(Integer cartId);
 }
